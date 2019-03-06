d2087ab25656392b1a10dc08fcdc1fa27bd2aeab
Brian <bbirdwell@gmail.com>
2019-03-06 10:46:56 -0800

    add images and styling to Home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import select_channel from '../images/select_channel.png'
+import select_show_back from '../images/select_show_back.png'
+
 
 const Home = (props) =>
 	<div>		
 		<h4>The simplest, easiest-to-use Acupuncture Study App...</h4>
 		<br/>
-		<div class="ml-3">
-			<p class="text-left">Choose a Channel</p>
-			<p class="text-left">Click 'Show/Hide Back' to see the "back" of the flashcard and reveal an image and description of the point's location</p>
+		<div class="text-left ml-3">
+			<p>Choose a Channel</p>
+			<img src={select_channel} height="100" width="400" />
+			<p>Click 'Show/Hide Back' to see the "back" of the flashcard and reveal an image and description of the point's location</p>
+			<img src={select_show_back} height="200" width="400" />
 		</div>
 	</div>
 
