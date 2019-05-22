ad2b35e7a04f1c5acfb976ef2123d5d4baf540aa
Great_Divide <bbirdwell+git@gmail.com>
2019-05-22 09:32:16 -0700

    create Auth.js file, add boilerplate code from auth0

--- client/src/auth/config.js
+++ client/src/auth/config.js
@@ -1,5 +1,5 @@
-export const config = {
-    clientID: 'YOUR_CLIENT_ID',
-    domain: 'YOUR_AUTH0_DOMAIN.auth0.com',
-    redirectUri: 'http://localhost:3000/callback'
-};
+// export const config = {
+//     clientID: 'YOUR_CLIENT_ID',
+//     domain: 'YOUR_AUTH0_DOMAIN.auth0.com',
+//     redirectUri: 'http://localhost:3000/callback'
+// };
